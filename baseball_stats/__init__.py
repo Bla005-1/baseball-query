9b833527dfe65201ff1291d034125c57ba3d408c
Bla005-1 <blasupercell@gmail.com>
2024-08-02 18:15:19 -0400

    version bump

--- baseball_stats/__init__.py
+++ baseball_stats/__init__.py
@@ -1,5 +1,5 @@
-from .batter_data import get_batter_data
-from .pitch_data import get_pitcher_data
+from .batter_data import get_batter_data, process_batter_rows
+from .pitch_data import get_pitcher_data, process_pitcher_rows
 from .common_data import get_combined_data
 from .utils import create_league_average_table, QueryBuilder, PlaysBuilder, TotalsBuilder
 from .db import daily_update, create_table
